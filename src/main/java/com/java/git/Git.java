a10b066efd9803c3204ba5029a194748ef01723d
XiaoYang <xiaoyang020305@outlook.com>
2024-07-02 15:59:28 +0800

    hot-fix commit1

--- src/main/java/com/java/git/Git.java
+++ src/main/java/com/java/git/Git.java
@@ -13,5 +13,6 @@ public class Git {
         System.out.println("Hello Git!");
         System.out.println("Hello git2");
         System.out.println("Hello git3");
+        System.out.println("Hello git4");
     }
 }
