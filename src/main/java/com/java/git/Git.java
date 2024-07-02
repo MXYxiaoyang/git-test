5fd4a9d0fb16176f799cf3246e90422099365894
XiaoYang <xiaoyang020305@outlook.com>
2024-07-02 15:53:33 +0800

    third commit

--- src/main/java/com/java/git/Git.java
+++ src/main/java/com/java/git/Git.java
@@ -12,5 +12,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello git2");
+        System.out.println("Hello git3");
     }
 }
