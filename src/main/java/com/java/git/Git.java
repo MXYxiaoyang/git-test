7401608f3f4ffdf3c716b2e687dfe3888dd27b13
XiaoYang <xiaoyang020305@outlook.com>
2024-07-02 15:52:57 +0800

    second commit

--- src/main/java/com/java/git/Git.java
+++ src/main/java/com/java/git/Git.java
@@ -11,5 +11,6 @@ package com.java.git;
 public class Git {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello git2");
     }
 }
