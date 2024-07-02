5539960a2ef9be60731775ec16030c310c1adab6
XiaoYang <xiaoyang020305@outlook.com>
2024-07-02 15:50:55 +0800

    first commit

---
+++ src/main/java/com/java/git/Git.java
@@ -0,0 +1,15 @@
+package com.java.git;
+
+/**
+ * ClassName:Git
+ * Package:com.java.git
+ * Description:
+ *
+ * @Author:XiaoYAng :2024/7/2 - 15:48
+ * @Version:v1.8
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
